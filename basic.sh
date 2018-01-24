d42458f85f3c15a1bfc11604227664bf90ec80a0
Arthur Schneider <dev.schneiart@gmail.com>
2018-01-24 23:24:35 +0100

    Add script which show how to print to the terminal

---
+++ basic.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+# The first line should be always a shebang
+# And it tells which shell should be used to interpret this script
+
+
+
+# Output
+# to output something from the script use echo command
+echo "Hello world!"
+
+# to output the filename of this script
+echo
+echo "This file is called $0"
+
+# to output the PID of this prozess
+# when a script is launched everytime a new shell will be opend and the script will be executed in
+# this seperate shell
+echo
+echo "The PID of the actual shell $$"
+
+#to output the total amount of input parameters of this script
+echo 
+echo "Total amount of input parameters $#"
+
+# to output first and second parameter given to this script
+# to see the result call the script with ./basic.sh a b
+echo
+echo "First argument  = $1 and the second argument = $2"
+
+# to output all arguments
+echo
+echo "All arguments passed to this script = $@"
+echo "OR"
+echo "All arguments passed to this script = $*"
